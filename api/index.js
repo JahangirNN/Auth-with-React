7de22f8fbcad5c35618281897098e10c5d229cf9
JahangirNN <acerdesktop53@gmail.com>
2024-03-09 01:07:50 +0530

    Created sign-up API route

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv' 
-import userRoutes from './routes/user.route.js'
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("Connected To MongoDB")
+    console.log("Connected To MongoDB");
 }).catch((err)=>{
-    console.log(err)
+    console.log(err);
 });
  
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, ()=>{
     console.log('Listening on Port 3000');
@@ -23,3 +26,4 @@ app.get('/', (req, res)=> {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
