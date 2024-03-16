af774874a3f5c14579dec75cee1e279cd56ff5e3
JahangirNN <acerdesktop53@gmail.com>
2024-03-16 16:53:05 +0530

    complete Update User Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,19 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useRef, useState } from "react";
 import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import {app} from '../firebase';
+import { updateUserFailure, updateUserStart, updateUserSuccess } from "../redux/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef(null);
   const [image, SetImage] = useState(undefined);
-  const {currentUser} = useSelector((state)=>state.user);  
+  const {currentUser, loading, error} = useSelector((state)=>state.user);  
   const [imagePercent, SetImagePercent] = useState(0);
   const [imageError, SetImageError] = useState(false);
   const [formData, SetFormData] = useState({});
-  
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch();
+
   useEffect(() => {
     if(image) {
       handleImageUpload(image);
@@ -38,15 +41,42 @@ const Profile = () => {
       }
     )
   }
+  const handleSubmit = async (e) => {
+    try
+      {
+        dispatch(updateUserStart());
+        e.preventDefault();
+        const response = await fetch(`/api/user/update/${currentUser._id}`, {
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body: JSON.stringify(formData)
+        });
+        
+        const data = await response.json();
+        if (data.success === false){
+          dispatch(updateUserFailure(data));
+          return;
+        }
+        dispatch(updateUserSuccess(data));
+        setUpdateSuccess(true);
+      }
+    catch(err){
+      dispatch(updateUserFailure(err));
+    }
+  }
+  const handleChange = (e) => {
+    SetFormData({...formData, [e.target.id]:e.target.value})
+  };
   
-
   return (    
-    <div className='dark:h-screen dark:bg-slate-700'>
-      <div className='h-screen p-3 max-w-lg mx-auto '>
+    <div className=' dark:bg-slate-700'>
+      <div className='p-3 max-w-lg mx-auto '>
 
         <h1 className="text-3xl font-bold text-center my-7 dark:text-white">Profile</h1>
 
-        <form className="flex flex-col gap-5 ">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-5 ">
           
           <input accept="image/*" hidden type="file" ref={fileRef} onChange={(e) => SetImage(e.target.files[0])}/>
 
@@ -57,25 +87,25 @@ const Profile = () => {
               <span className='text-red-700'>
                 Error uploading image (file size must be less than 2 MB)
               </span>
-            ) :imagePercent === 100 ? <span className='text-green-700'>Image uploaded successfully</span> : imagePercent >0 && imagePercent < 100 ? <span className='text-green-700'>{`Uploading ${imagePercent} %`}</span> : '' }
+            ) :imagePercent === 100 ? <span className='text-green-700'>Image uploaded successfully</span> : imagePercent >0 && imagePercent < 100 ? <span className='text-green-700 '>{`Uploading ${imagePercent} %`}</span> : '' }
           </p>
 
           <input defaultValue={currentUser.username} type='text' placeholder='Username'
           id='username' className='bg-slate-100 p-3
-          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110' />          
+          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110'  onChange={handleChange} />          
           
           <input defaultValue={currentUser.email} type='text' placeholder='email'
           id='email' className='bg-slate-100 p-3
-          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110' />
+          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110'  onChange={handleChange}/>
           
           <input  type='text' placeholder='password'
           id='password' className='bg-slate-100 p-3
-          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110' />
+          rounded-lg shadow-md transform transition duration-300 ease-in-out hover:scale-110'  onChange={handleChange}/>
 
           <button className='border-4 border-solid border-black  
           bg-slate-700 text-white p-3
-            rounded-lg uppercase hover:opacity-75 font-semibold
-            disabled:opacity-60 dark:border-white'>Update
+          rounded-lg uppercase hover:opacity-85 font-semibold
+          disabled:opacity-60 dark:border-white'>{loading? 'UPdating...':'UPDATE'}
           </button>
 
           <div className="flex justify-between mt-1">
@@ -84,7 +114,11 @@ const Profile = () => {
           
             <span className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-3 py-2.5 text-center me-5 mb-5">Sign-out</span>
           </div>
-        </form>
+
+          </form>
+          
+          <p className="text-red-500" >{error && 'Something went wrong'}</p>
+          <p className="text-green-500 self-center" >{updateSuccess && 'User Updated'}</p>
       </div>
     </div>
   )
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -21,10 +21,22 @@ const userSlice = createSlice({
     singInFailure: (state, action) => {
         state.loading = false;
         state.error = action.payload;        
-    }
+    },
+    updateUserStart:(state)=>{
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) =>{
+      state.loading = false;
+      state.error = false;
+      state.currentUser = action.payload
+    },
+    updateUserFailure: (state, action) =>{
+      state.loading = false;
+      state.error = action.payload;
+    },
   }
 });
 
-export const {signInStart, signInSuccess, singInFailure} = userSlice.actions;
+export const {signInStart, signInSuccess, singInFailure, updateUserFailure, updateUserStart, updateUserSuccess} = userSlice.actions;
 
 export default userSlice.reducer;
